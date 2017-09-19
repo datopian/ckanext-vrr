28446090d31e53b41883f496901598abc1e621d3
Goran Maksimovski <goran.maksimovski@keitaro.com>
2017-09-19 12:57:07 +0200

    Text & various UI changes

--- ckanext/vrr/controllers/vrr.py
+++ ckanext/vrr/controllers/vrr.py
@@ -1,8 +1,12 @@
 from ckan.lib import base
+import ckan.plugins.toolkit as toolkit
 render = base.render
 BaseController = base.BaseController
 
 
 class VrrController(BaseController):
     def api(self):
-        return render('api.html')
+        controller = 'ckanext.pages.controller:PagesController'
+        action = 'pages_show'
+        page = '/api'
+        return toolkit.redirect_to(controller=controller, action=action, page=page)
