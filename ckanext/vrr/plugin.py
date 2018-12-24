183d4706306122ab17f3eff536a9ff8b05776c56
Jovanka <JGulic@users.noreply.github.com>
2018-12-24 10:24:31 +0100

    Remove GDPR changes (#2)

--- ckanext/vrr/plugin.py
+++ ckanext/vrr/plugin.py
@@ -1,8 +1,5 @@
 import ckan.plugins.toolkit as toolkit
 import ckan.plugins as p
-import ckanext.vrr.action
-from ckan.config.routing import SubMapper
-
 
 
 if p.toolkit.check_ckan_version(min_version='2.5'):
@@ -17,7 +14,6 @@ else:
 class VrrPlugin(VrrPluginBase):
     p.implements(p.IConfigurer)
     p.implements(p.IRoutes, inherit=True)
-    p.implements(p.IActions)
 
 
     # IConfigurer
@@ -38,22 +34,4 @@ class VrrPlugin(VrrPluginBase):
                     '/api',
                     controller=vrr_controller,
                     action='api')
-        user_ctrl = 'ckanext.vrr.controllers.vrr:VrrUserController'
-        with SubMapper(map, controller=user_ctrl) as m:
-            m.connect('register', '/user/_register_partner', action='register')
-            map.connect('user_index', '/user',
-                        controller=user_ctrl, action='index')
-            m.connect('/user/activity/{id}/{offset}', action='activity')
-            m.connect('user_activity_stream', '/user/activity/{id}',
-                      action='activity', ckan_icon='time')
         return map
-
-    def get_actions(self):
-        action_functions = {
-            'user_list':
-                ckanext.vrr.action.user_list,
-            'user_show':
-                ckanext.vrr.action.user_show
-        }
-
-        return action_functions
